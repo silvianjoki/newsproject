eb86bf0a247c7c69268233c1f2b286fdafefa2c7
silvianjoki <10silvianjoki@gmail.com>
2022-03-02 11:25:56 +0300

    configure app requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,29 @@
 from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-
+# initializing bootstrap extension
 bootstrap = Bootstrap
 
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    # creating app configurations
+    app.config.from_object(config_options[config_name])
+    
+    # initalizing flask extensions
+    bootstrap.init_app(app)
+    
+    # registering blueprint 
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+    
+    return app
 
-app = Flask(__name__)
+
