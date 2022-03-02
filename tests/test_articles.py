6c90a5c0aec9cab1edee8b8c6d53082ef03a0a1f
silvianjoki <10silvianjoki@gmail.com>
2022-03-02 12:01:43 +0300

    edits the tests

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -7,7 +7,6 @@ class articlesTest(unittest.TestCase):
     '''
     Test class to test behavior of news class
     '''
-    
     def setUp(self):
         self.new_articles = Articles('BBC News', 'People have swapped their pens and keyboards for guns', 
         'Thousands of Ukrainian citizens are volunteering to fight, including many with no military experience.',
@@ -15,13 +14,13 @@ class articlesTest(unittest.TestCase):
         'https://ichef.bbci.co.uk/news/1024/branded_news/11C59/production/_123439727_p0brgxj5.jpg', 
         '2022-02-27T13:37:22.9175529Z'
         'As the Russian invasion continues, thousands of ordinary Ukrainians are volunteering to fight to defend their neighbourhoods, despite many having no previous military experience. \r\nMen between the ag… [+369 chars]')
-        
+
     def test_instance(self):
         '''
         Test case to test if object is an instance of the class
         '''
         self.assertTrue(isinstance(self.new_articles, Articles))
-        
+
     def test_init(self):
         '''
         Test initialization tests if the object is properly initialized
@@ -33,6 +32,6 @@ class articlesTest(unittest.TestCase):
         self.asserrEqual(self.new_articles.urlToImage, 'https://ichef.bbci.co.uk/news/1024/branded_news/11C59/production/_123439727_p0brgxj5.jpg')
         self.asserrEqual(self.new_articles.publishedAt, '2022-02-27T13:37:22.9175529Z')
         self.asserrEqual(self.new_articles.content, 'As the Russian invasion continues, thousands of ordinary Ukrainians are volunteering to fight to defend their neighbourhoods, despite many having no previous military experience. \r\nMen between the ag… [+369 chars]')
-        
+
 if __name__ == '__main__':
     unittest.main
--- tests/test_news.py
+++ tests/test_news.py
@@ -7,16 +7,15 @@ class newsTest(unittest.TestCase):
     '''
     Test class to test behavior of news class
     '''
-    
     def setUp(self):
         self.new_news = News('bbc-news', 'BBC News', 'Use BBC News for up-to-the-minute news, breaking news, video, audio and feature stories. BBC News provides trusted World and UK news as well as local and regional perspectives. Also entertainment, business, science, technology and health news.', '"http://www.bbc.co.uk/news"', 'general', 'en', 'gb')
-        
+
     def test_instance(self):
         '''
         Test case to test if object is an instance of the class
         '''
         self.assertTrue(isinstance(self.new_news, News))
-        
+
     def test_init(self):
         '''
         Test initialization tests if the object is properly initialized
