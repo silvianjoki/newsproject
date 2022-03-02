56b5d12431674f9a96480313ad1a8d230ef448e1
silvianjoki <10silvianjoki@gmail.com>
2022-03-02 14:27:04 +0300

    edit config to include secret key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from distutils.debug import DEBUG
+from ast import Pass
 import os
 
 
@@ -10,11 +10,13 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&sortBy=popularity&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
 class ProdConfig(Config):
     '''
     Configuration for child class-production
     '''
+    pass
     
 class DevConfig(Config):
     '''
